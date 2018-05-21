512fa8f96377c47def5d3a3cd57f30e9293169f8
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2018-05-21 16:39:07 -0500

    Add files via upload

---
+++ KNN_210518.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dataset=${1?Error: no data given}
+k=${2?Error: no k given}
+
+java -jar elki-bundle-0.7.1.jar KDDCLIApplication -verbose -dbc.in $dataset -algorithm outlier.distance.KNNOutlier -knno.k $k -out myresults/ 
+
+head -10 myresults/knn-outlier_order.txt
+
