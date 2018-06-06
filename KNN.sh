dfc55a51b5bbc536ee603af86007ebee44c37cc9
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2018-06-06 16:28:03 -0500

    Delete KNN.sh

--- KNN.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dataset=${1?Error: no data given}
-k=${2?Error: no k given}
-
-java -jar elki-bundle-0.7.1.jar KDDCLIApplication -verbose -dbc.in $dataset -algorithm outlier.distance.KNNOutlier -knno.k $k
