81cc55ae511ab83a19c47ece852f55da59d81841
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2018-06-06 16:21:27 -0500

    Delete CLI_ELKI_0.2.sh

--- CLI_ELKI_0.2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-java -jar elki-bundle-0.7.1.jar KDDCLIApplication -dbc.in mydata/mouse.csv -algorithm clustering.kmeans.KMedoidsEM -kmeans.k 1 -resulthandler ResultWriter -out.gzi -out output/k-1
-
-
